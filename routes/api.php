1f6e9083d6d6ccde78575284d2554a96dba1867b
Syauqi <sauki084@gmail.com>
2025-04-28 07:44:53 +0700

    Create controller & route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Belajar;
+use App\Http\Controllers\TaxiTripController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,6 @@ Route::get('/user', function (Request $request) {
 Route::get('test_api', [Belajar::class, 'coba']);
 Route::post('cek_hari', [Belajar::class, 'cek_tgl']);
 Route::get('set_rahasia/{jenis}/{teks}', [Belajar::class, 'enkripsi_deskripsi']);
+
+
+Route::get('/rekap-taxi', [TaxiTripController::class, 'rekap']);
